e7dcbc4020ebcd27ce2be36cb9692a83001de96f
Magabole Tsoka <mtsoka021@student.wethinkcode.co.za>
2023-05-18 21:51:48 +0200

    User can enter name now

--- src/project/main/java/org/example/Main.java
+++ src/project/main/java/org/example/Main.java
@@ -1,11 +1,34 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.InputStream;
+import java.util.Scanner;
 
+public class Main {
+    private String name;
+    private Scanner scanner;
+
+    public static void main(String[] args) {
+        print("Welcome to B-Cal");
+    }
+
+    public void initialise() {
+        initialise(System.in);
+    }
+
+    public void initialise(InputStream inputStream) {
+        scanner = new Scanner(inputStream);
     }
 
     public static void print(String stmt) {
         System.out.println(stmt);
+    }
+
+    public void setName() {
+        print("What is your name? ");
+        name = scanner.nextLine();
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/project/test/java/org/example/TestMainClass.java
+++ src/project/test/java/org/example/TestMainClass.java
@@ -13,21 +13,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestMainClass {
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
+    private Main main;
     @BeforeEach
     void setUp() {
+        main = new Main();
         System.setOut(new PrintStream(outputStream));
     }
 
     @AfterEach
     void tearDown() {
+        main = null;
         System.setOut(System.out);
     }
 
     @Test
     void testPrint() {
         Main.print("Welcome to B-Cal");
-        assertEquals("Welcome to B-Cal", outputStream.toString().trim());
+        assertEquals("Welcome to B-Cal\n", outputStream.toString());
+    }
+
+    @Test
+    void testSetName() {
+        main.initialise(generateInputStream("Simon"));
+        main.setName();
+        assertEquals("Simon", main.getName());
     }
 
     private InputStream generateInputStream(String userInput) {
